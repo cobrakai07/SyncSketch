139707fe28b50d44717005bc82f190090d0dd765
Kai <dk786161616@gmail.com>
2024-05-22 03:09:43 +0530

    fixed clerk auth

--- my-app/app/(dashboard)/_components/NavBar.tsx
+++ my-app/app/(dashboard)/_components/NavBar.tsx
@@ -1,19 +1,16 @@
 "use client";
-import React from 'react'
+import React from "react";
 import { UserButton } from "@clerk/nextjs";
 
 function NavBar() {
   return (
-    <div className='flex items-center gap-x-4 p-5 bg-green-400'>
-        <div className='hidden lg:flex lg:flex-1 bg-yellow-400'>
-            search
-        </div>
-        <div>
-          //Clerk//
-        <UserButton/>
-        </div>
+    <div className="flex items-center gap-x-4 p-5 bg-green-400">
+      <div className="hidden lg:flex lg:flex-1 bg-yellow-400">search</div>
+      <div>
+        <UserButton />
+      </div>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -1,8 +1,19 @@
 
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// export default clerkMiddleware();
+
+// export const config = {
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
+
+const isProtectedRoute = createRouteMatcher(["/"])
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect()
+})
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+}
